a86d9d58ac04db881a02b0ae6e7b26fe8cea7b45
17200128 <58438481+17200128@users.noreply.github.com>
2020-04-08 00:02:18 +0400

    full

--- app/src/main/java/ge/edu/simpleintent/MainActivity.java
+++ app/src/main/java/ge/edu/simpleintent/MainActivity.java
@@ -5,8 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView mWelcome;
+    private Button mEnter;
+    public TextView mDescr;
+    public Button mSubmit;
+    public TextView mCongr;
+    public TextView mSeescore;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,4 +31,38 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
+    public void geo(View view) {
+        mWelcome=findViewById(R.id.welcome);
+        String welcome = "მოგესალმებით ქვიზზე!";
+        mWelcome.setText(welcome);
+
+        mEnter=findViewById(R.id.enter);
+        String enter = "შესვლა";
+        mEnter.setText(enter);
+
+        Button geo =(findViewById(R.id.geo)) ;
+        geo.setVisibility(View.GONE);
+        Button eng =(findViewById(R.id.eng)) ;
+        eng.setVisibility(View.VISIBLE);
+
+
+    }
+
+
+
+    public void eng(View view) {
+
+        mWelcome=findViewById(R.id.welcome);
+        String welcome = "Welcome to Quiz!";
+        mWelcome.setText(welcome);
+
+        mEnter=findViewById(R.id.enter);
+        String enter = "Enter";
+        mEnter.setText(enter);
+
+        Button geo =(findViewById(R.id.geo)) ;
+        geo.setVisibility(View.VISIBLE);
+        Button eng =(findViewById(R.id.eng)) ;
+        eng.setVisibility(View.GONE);
+    }
 }
